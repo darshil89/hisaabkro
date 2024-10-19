e47acd0afd1bd6aed1c7fc5c79050ddbee3f5ce3
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-10-19 23:35:53 +0530

    updated the  layout and thr font

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,51 +4,30 @@ import "../globals.css";
 import Provider from "@/libs/Provider";
 import Navbar from "@/components/customUI/Navbar";
 
-
-const geistSans = localFont({
-    src: "../fonts/GeistVF.woff",
-    variable: "--font-geist-sans",
-    weight: "100 900",
-});
-const geistMono = localFont({
-    src: "../fonts/GeistMonoVF.woff",
-    variable: "--font-geist-mono",
-    weight: "100 900",
-});
-
 export const metadata: Metadata = {
-    title: "Hisaab Kro",
-    description: "Split expenses with friends and family",
+  title: "Hisaab Kro",
+  description: "Split expenses with friends and family",
 };
 
 export default async function RootLayout({
-    children,
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <Provider>
-                <div className="min-h-screen white flex justify-center items-center">
-                    <div className="relative w-4/5 h-screen md:w-2/3 lg:w-1/2 p-6 rounded-lg bg-slate-50">
-                        <div className="absolute inset-0 pointer-events-none rounded-lg border-[0px] border-transparent box-border" style={{
-                            boxShadow: `
-                            0 0 15px 15px white,   
-                            0 0 15px 15px white inset, 
-                            15px 0 15px 15px white,    
-                            -15px 0 15px 15px white inset 
-                        `
-                        }}></div>
-                        <Navbar />
-                        <hr className="border-0 h-1 bg-gradient-to-r from-transparent via-gray-300 to-transparent mb-6" />
-                        <div
-                            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-                        >
-                            {children}
-                        </div>
-                    </div>
-                </div>
-            </Provider>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <Provider>
+        <div className="min-h-screen white flex justify-center items-center">
+          <div className="relative w-4/5 h-screen md:w-2/3 lg:w-1/2 p-6 rounded-lg bg-sky-50">
+            <div className="absolute inset-0 pointer-events-none rounded-lg border-[0px] border-transparent box-border"></div>
+            <Navbar />
+            <hr className="border-0 h-1 bg-gradient-to-r from-transparent via-gray-300 to-transparent mb-6" />
+            <div className="font-permanent-marker">
+              {children}
+            </div>
+          </div>
+        </div>
+      </Provider>
+    </html>
+  );
 }
--- src/components/customUI/Navbar.tsx
+++ src/components/customUI/Navbar.tsx
@@ -7,23 +7,23 @@ const Navbar: FC = () => {
     const [activeTab, setActiveTab] = useState("dashboard");
 
     return (
-        <nav className="flex justify-between text-center mb-6 pb-2">
+        <nav className="flex font-permanent-marker justify-between text-center mb-6 pb-2">
             <Link href="/dashboard"
-                className={`w-1/3 pb-2 ${activeTab === "dashboard" ? "font-bold" : "font-normal"
+                className={`w-1/3 pb-2 text-2xl ${activeTab === "dashboard" ? "font-bold" : "font-normal"
                     }`}
                 onClick={() => setActiveTab("dashboard")}
             >
                 Dashboard
             </Link>
             <Link href="/dashboard/profile"
-                className={`w-1/3 pb-2 ${activeTab === "profile" ? "font-bold" : "font-normal"
+                className={`w-1/3 pb-2 text-2xl ${activeTab === "profile" ? "font-bold" : "font-normal"
                     }`}
                 onClick={() => setActiveTab("profile")}
             >
                 Profile
             </Link>
             <Link href="/dashboard/expense"
-                className={`w-1/3 pb-2 ${activeTab === "expense" ? "font-bold" : "font-normal"
+                className={`w-1/3 pb-2 text-2xl ${activeTab === "expense" ? "font-bold" : "font-normal"
                     }`}
                 onClick={() => setActiveTab("expense")}
             >
