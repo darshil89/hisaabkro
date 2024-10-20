c75ca761ef9032ce586e8f8875cd53460bbc5ded
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-10-20 19:44:11 +0530

    updated the UI

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -21,7 +21,7 @@ export default async function RootLayout({
           <div className="relative w-full md:w-4/5 h-screen lg:w-1/2 p-6 rounded-lg">
             <div className="absolute inset-0 pointer-events-none rounded-lg border-[0px] border-transparent box-border"></div>
             <Navbar />
-            <hr className="border-0 h-1 bg-gradient-to-r from-transparent via-black to-transparent mb-6" />
+            <hr className="border-0 h-1 bg-gradient-to-r from-transparent via-black to-transparent mb:2 md:mb-6" />
             <div className="font-permanent-marker">
               {children}
             </div>
