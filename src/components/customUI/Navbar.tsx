be3092969a387a4cdc23afc9633ba431a7e2a595
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-10-20 19:32:54 +0530

    made the navbar responsive

--- src/components/customUI/Navbar.tsx
+++ src/components/customUI/Navbar.tsx
@@ -11,23 +11,23 @@ const Navbar: FC = () => {
     const [activeTab, setActiveTab] = useState(activetab);
 
     return (
-        <nav className="flex font-permanent-marker justify-between text-center mb-6 pb-2">
+        <nav className="flex flex-col md:flex-row font-permanent-marker justify-between text-center mb-6 pb-2">
             <Link href="/dashboard"
-                className={`w-1/3 text-gray-900 pb-2 text-2xl ${activeTab === "dashboard" ? "font-semibold" : "font-normal"
+                className={`w-full md:w-1/3 text-gray-900 pb-2 text-2xl ${activeTab === "dashboard" ? "font-semibold" : "font-normal"
                     }`}
                 onClick={() => setActiveTab("dashboard")}
             >
                 Dashboard
             </Link>
             <Link href="/dashboard/profile"
-                className={`w-1/3 pb-2 text-2xl ${activeTab === "profile" ? "font-semibold" : "font-normal"
+                className={`w-full md:w-1/3 pb-2 text-2xl ${activeTab === "profile" ? "font-semibold" : "font-normal"
                     }`}
                 onClick={() => setActiveTab("profile")}
             >
                 Profile
             </Link>
             <Link href="/dashboard/expense"
-                className={`w-1/3 pb-2 text-2xl ${activeTab === "expense" ? "font-semibold" : "font-normal"
+                className={`w-full md:w-1/3 pb-2 text-2xl ${activeTab === "expense" ? "font-semibold" : "font-normal"
                     }`}
                 onClick={() => setActiveTab("expense")}
             >
